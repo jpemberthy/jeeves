4151867d6fd3d4ecaf0153ca5c7479e1d0e65319
Juan Pemberthy <juanp@uber.com>
2016-01-24 18:13:40 -0800

    Initial commit.

---
+++ jeeves.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+const watchedLog = "/Applications/Hearthstone/Logs/Power.log"
+
+func main() {
+	file, err := os.Open(watchedLog)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	file.Seek(0, 2) // go to the end of file.
+	reader := bufio.NewReader(file)
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	for range ticker.C {
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				} else {
+					log.Fatal(err)
+				}
+			}
+			log.Println(line)
+		}
+	}
+}
