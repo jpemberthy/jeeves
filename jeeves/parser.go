04478f7b34948dc995012a8e6d0a6f57bc45e6e7
Juan Pemberthy <juanp@uber.com>
2016-01-27 23:39:12 -0800

    Add some godoc.
    TODO: ci/lint.

--- jeeves/parser.go
+++ jeeves/parser.go
@@ -1,6 +1,9 @@
 package jeeves
 
-import "regexp"
+import (
+	"errors"
+	"regexp"
+)
 
 // Result represents a hearthstone basic game result.
 type Result struct {
@@ -8,6 +11,8 @@ type Result struct {
 	Status string
 }
 
+// ParseGameResult parses a string and detects a winner/loser.
+// If no result can be detected, an error is returned.
 func ParseGameResult(line string) (Result, error) {
 	result := Result{}
 	r, err := regexp.Compile("Entity=([^\t\n\f\r ]*).*value=(WON|LOST)")
@@ -20,7 +25,9 @@ func ParseGameResult(line string) (Result, error) {
 		matches := r.FindStringSubmatch(line)
 		result.Player = matches[1]
 		result.Status = matches[2]
+		return result, nil
 	}
 
-	return result, nil
+	err = errors.New("can't detect game result")
+	return result, err
 }
